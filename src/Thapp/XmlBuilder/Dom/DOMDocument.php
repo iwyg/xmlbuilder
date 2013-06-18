bd01b96b03a4df6e1ade8ec25bd666de788630e2
Thomas Appel <mail@thomas-appel.com>
2013-06-18 14:58:33 +0200

    create new xpath when cloning the document

--- src/Thapp/XmlBuilder/Dom/DOMDocument.php
+++ src/Thapp/XmlBuilder/Dom/DOMDocument.php
@@ -43,6 +43,19 @@ class DOMDocument extends BaseDom
     }
 
     /**
+     * __clone
+     *
+     * @access public
+     * @return void
+     */
+    public function __clone()
+    {
+        if ($this->xpath) {
+            $this->xpath = new DOMXpath($this);
+        }
+    }
+
+    /**
      * xPath
      *
      * @param mixed $query
